0f508897ad2d64d557c6e6c54131cda7d46535e8
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2022-12-12 12:22:48 +0500

    12-12-2022

---
+++ 12-12-2022/index.js
@@ -0,0 +1,25 @@
+// var number=+prompt("Enter a number to start")
+
+// while(number<100){
+//    if(number%2===0){
+//        document.write(number+"<br>")
+//    }
+//    number++
+// }
+
+// do{
+//     if(number%2===0){
+//         document.write(number+"<br>")
+//     }
+//     number++
+//  }while(number<100)
+
+for(var i=0;i<=15;i++){
+    if(i%2===0){
+        document.write(i+" is even"+"<br>")
+          }
+          else if(i%2!==0){
+            document.write(i+" is odd"+"<br>")
+              }
+          
+}
